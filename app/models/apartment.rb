bc72cfc8106520f4467a4c8508b82c6f74deef1a
Blake Johnson <blake41@gmail.com>
2014-04-18 10:53:52 -0400

    updated locations to use a regex to find the good spots

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -2,12 +2,25 @@ class Apartment < ActiveRecord::Base
   attr_accessible :bedrooms, :href, :location, :price, :description, :image_url
   
   LOCATIONS = ["Carroll Gardens", "Cobble Hill / Carroll Gardens", 
-    "Cobble Hill", "CARROLL GARDENS", "Carroll Gardents", 
+    "Cobble Hill", "CARROLL GARDENS", 
     "Brooklyn Heights - Atlantic Ave & Hicks St", "BOERUM HILL", 
     "Boerum Hill", "brooklyn heights"] 
   
   def self.good
+    Apartment.where(:location => find_good_hoods)
+  end
+
+  def self.old_good
     Apartment.where(:location => LOCATIONS)
+  end
+
+  def self.find_good_hoods
+    good_hoods = []
+    Apartment.find_by_sql("select distinct location from apartments").collect {|a|a.location}.each do |hood|
+      match = hood.match(/Carroll.*Gardens.*|.*Cobble.*Hill.*|.*BOERUM.*HILL.*|.*brooklyn.*heights.*|.*CARROLL.*GARDENS.*/i)
+      good_hoods << match.to_s if match
+    end
+    good_hoods
   end
 
   def formatted_url
